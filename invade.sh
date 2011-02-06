7fae71a7b8488bb437cc2bee6c2ff2d4db1afcb6
blandinw <wblandin@gmail.com>
2011-02-06 20:34:16 +0100

    Added ackrc

--- invade.sh
+++ invade.sh
@@ -4,11 +4,13 @@ cd ~
 mv .bashrc .bashrc.old
 mv .zshrc .zshrc.old
 mv .vimrc .vimrc.old
+mv .ackrc .ackrc.old
 mv .gvimrc .gvimrc.old
 ln -s dotfiles/bashrc .bashrc
 ln -s dotfiles/zshrc .zshrc
 ln -s dotfiles/vim .vim
 ln -s .vim/vimrc .vimrc
 ln -s .vim/gvimrc .gvimrc
+ln -s dotfiles/ackrc .ackrc
 
 touch dotfiles/bash/local
