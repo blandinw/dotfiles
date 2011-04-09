8b7907a3e3ec9c5d161419ab59b59f7e309b90be
blandinw <wblandin@gmail.com>
2011-04-09 23:53:16 +0200

    Improved lua.dump display

--- init.lua
+++ init.lua
@@ -1,21 +1,22 @@
 -- quick debug functions
 function dump(o, indent)
-    indent = indent or 0
+    if(not indent) then indent = 0 end
 
     if type(o) == 'table' then
         local s = '{\n'
+
+        -- prepare the right indentation
+        local indentstring = ''
+        for i = 0, indent do
+            indentstring = indentstring .. "    "
+        end
+
         for k,v in pairs(o) do
             if type(k) ~= 'number' then k = '"'..k..'"' end
 
-            -- prepare the right indentation
-            local indentstring = ''
-            for i = 0, indent do
-                indentstring = indentstring .. "\t"
-            end
-
             s = s .. indentstring .. '['..k..'] = ' .. dump(v, indent + 1) .. '\n'
         end
-        return s .. string.sub(indentstring, 1, -2) .. '}'
+        return s .. string.sub(indentstring, 1, -5) .. '}'
     else
         return tostring(o)
     end
