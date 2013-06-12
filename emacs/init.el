9c82ee7dfe90d6e96a293d2c1547bb4cf947cc52
Willy Blandin <wblandin@gmail.com>
2013-06-11 20:09:12 -0700

    amount of stuff

---
+++ emacs/init.el
@@ -0,0 +1,17 @@
+(add-to-list 'load-path "~/.emacs.d")
+(load "willy.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(safe-local-variable-values (quote ((erlang-indent-level . 4))))
+ '(send-mail-function (quote mailclient-send-it)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'dired-find-alternate-file 'disabled nil)
