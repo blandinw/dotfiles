cfedfec4065aedac543c9154f9b61687281c4638
Willy Blandin <willy@fb.com>
2016-03-27 11:32:04 -0700

    [bin] linux killport

--- bin/killport.sh
+++ bin/killport.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 if [ $# -lt 1 ]; then
     echo "Usage: $(basename $0) <port> <signal>"
     exit
@@ -14,4 +16,14 @@ else
     signal="INT"
 fi
 
-lsof -i ":$port" | tail -n+2 | awk '{print $2}' | xargs kill "-$signal"
+PLATFORM=$(uname)
+if [ $PLATFORM = Darwin ]; then
+  HITS=$(lsof -i ":$port" | tail -n+2 | awk '{print $2}')
+elif [ $PLATFORM = Linux ]; then
+  HITS=$(sudo netstat -nltp | grep ":$port" | awk '{print $7}' | egrep -o '[0-9]+' | uniq)
+else
+  echo "error: unk platform $PLATFORM"
+  exit 1
+fi
+
+echo $HITS | xargs kill "-$signal"
